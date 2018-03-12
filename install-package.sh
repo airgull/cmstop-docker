c2ecdcfe0f40afa9b1509ca3d441ef06bc787dde
Derrick <airgull@sina.com>
2018-03-12 17:56:33 +0800

    add php56 extension

---
+++ install-package.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -ex
+
+apk --no-cache add \
+    php5 \
+    php5-bcmath \
+    php5-ctype \
+    php5-curl \
+    php5-dom \
+    php5-ftp \
+    php5-gd \
+    php5-iconv \
+    php5-json \
+    php5-mcrypt \
+    php5-mysql \
+    php5-mysqli \
+    php5-openssl \
+    php5-pdo \
+    php5-pdo_sqlite \
+    php5-pear \
+    php5-phar \
+    php5-posix \
+    php5-shmop \
+    php5-snmp \
+    php5-sockets \
+    php5-sqlite3 \
+    php5-sysvsem \
+    php5-xml \
+    php5-xmlreader \
+    php5-xmlrpc \
+    php5-zip \
+    php5-zlib
